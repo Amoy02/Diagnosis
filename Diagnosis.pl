02b2883cded1056afd7834e48aba50218ca512aa
Clarke, Hakeem  J <clarkehaj@jncb.com>
2018-11-06 16:35:12 -0500

    Add credits

---
+++ Diagnosis.pl
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  COMP3220
+%  Assignment 2
+%  Diagnosis.pl                                                                                                   
+%                                                                                                                                   
+%  This knowledge base attempts to diagnose an illness based on symptoms then prescribe a medication for it
+%
+%  Authors: Amoy .. - 6200.....
+%           Hakeem Clarke - 620086999
+%           Ackeem Smith  - 6200....
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
